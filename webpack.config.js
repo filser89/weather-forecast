d8fc09ef8ba71c710b1d22d8cf2939c294685e77
Sergey Filippov <sergey.filippov.89@inbox.ru>
2021-11-04 22:36:02 +0800

    livecode on Nov 4

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./lib/index.js"),
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+};
